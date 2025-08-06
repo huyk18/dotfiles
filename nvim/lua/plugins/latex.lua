de1cf44ecfb0459c7db15a518daa7b23b2b58e6a
huyk18 <huyongkangxx@qq.com>
2025-08-06 17:38:40 +0800

    fix

--- nvim/lua/plugins/latex.lua
+++ nvim/lua/plugins/latex.lua
@@ -1,18 +1,17 @@
-return{
+return {
   -- add any tools you want to have installed below
   {
     "williamboman/mason.nvim",
     opts = function(_, opts)
-      table.insert(opts.ensure_installed, { "tex-fmt" })
+      table.insert(opts.ensure_installed, "tex-fmt")
     end,
   },
   {
     "stevearc/conform.nvim",
-    opts = function(_, opts)
-      table.insert(opts.formatters_by_ft, { tex= "tex-fmt" })
-    end,
-  }
+    opts = {
+      formatters_by_ft = {
+        tex = { "tex-fmt" },
+      },
+    },
+  },
 }
-
-
-
